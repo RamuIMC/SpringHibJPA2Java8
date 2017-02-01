8f82220d6c53e0be1bd863dbd1f137015d5b5344
Ramakrishna <mramakrishna.mrk@gmail.com>
2017-02-02 00:03:54 +0530

    Hazelcast

--- src/main/java/com/project/controller/UserInfoController.java
+++ src/main/java/com/project/controller/UserInfoController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.config.ClientNetworkConfig;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
 import com.project.dao.UserDao;
 import com.project.dto.UserDto;
 import com.project.model.User;
@@ -22,6 +27,19 @@ public class UserInfoController {
 		
 		UserDto userDto = new UserDto();
 		User user= userDao.getUserById(userId);
+		
+		ClientConfig clientConfig = new ClientConfig();
+		ClientNetworkConfig networkConfig = clientConfig.getNetworkConfig();
+		networkConfig.addAddress("192.168.100.5");
+		
+		HazelcastInstance client = HazelcastClient.newHazelcastClient( clientConfig );
+        IMap map = client.getMap("customers");
+        System.out.println( "Map Size:" + map.size() );
+        map.put( 1, "Joe" );
+        map.put( 2, "Ali" );
+        map.put( 3, "Avi" );
+       
+        System.out.println( "Map Size:" + map.size() );
 		
 		userDto.setUserId(user.getId());
 		userDto.setUserName(user.getUserName());
